b7678e08a1b0be1e56e75e7427a6ca51a4a2f61d
Abdullah Al Muhid <abdullahalmuhit1296@gmail.com>
2024-10-16 18:08:35 +0600

    Changes on old tin

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -116,9 +116,9 @@ class UserService
             'tax_payer_status' => $requestData['tax_payer_status'] ?? $existingData->tax_payer_status,
             'residential_status' => $requestData['residential_status'] ?? $existingData->residential_status,
             'tax_payer_location_id' => $requestData['tax_payer_location_id'] ?? $existingData->tax_payer_location_id,
-            'old_tin' => $requestData['old_tin'] ?? $existingData->old_tin,
-            'old_circle_id' => $requestData['old_circle_id'] ?? $existingData->old_circle_id,
-            'old_zone_id' => $requestData['old_zone_id'] ?? $existingData->old_zone_id,
+            'old_tin' => $requestData['old_tin'] ?? "",
+            'old_circle_id' => $requestData['old_circle_id'] ?? "",
+            'old_zone_id' => $requestData['old_zone_id'] ?? "",
             // family profile
             'marital_status' => $requestData['marital_status'] ?? $existingData->marital_status,
             'spouse_name' => $requestData['spouse_name'] ?? $existingData->spouse_name,
