9dae81338b535ddcedd077eaa058f93ca4eca859
mizanur090148 <mizanur090148@gmail.com>
2024-11-29 09:17:58 +0600

    total data relted tasks

--- app/Services/TotalDataService.php
+++ app/Services/TotalDataService.php
@@ -49,11 +49,15 @@ class TotalDataService
         $partnershipBusiness = PartnershipBusiness::where(['past_return' => 1, 'user_id' => $userId])->sum('closing_capital');
         $businessAsset = BusinessAsset::where(['past_return' => 1,'user_id' => $userId])->sum(DB::raw('total_assets - closing_liabilities'));
         $directoryShare = DirectoryShare::where(['past_return' => 1,'user_id' => $userId])->sum(DB::raw('closing_no_of_shares * cost_per_share'));
-        $nonAgriLand = AgriNonAgriLand::where(['past_return' => 1, 'type' => 'non-agri','user_id' => $userId])->sum('net_value_of_property');
-        $agriLand = AgriNonAgriLand::where(['past_return' => 1, 'type' => 'agri','user_id' => $userId])->sum('net_value_of_property');
-        $motorVehicle = MotorVehicle::where(['past_return' => 1,'user_id' => $userId])->sum('cost_with_registration');
-        $jewellery = Jewellery::where(['past_return' => 1,'user_id' => $userId])->sum('closing_value');
-        $furniture = FurnitureEquipment::where(['past_return' => 1,'user_id' => $userId])->sum('closing_value');
+        $nonAgriLand = AgriNonAgriLand::where(['past_return' => 1, 'type' => 'non-agri','user_id' => $userId])
+            ->sum(DB::raw('net_value_of_property + renovation_deployment - cost_of_sale_portion'));
+        $agriLand = AgriNonAgriLand::where(['past_return' => 1, 'type' => 'agri','user_id' => $userId])
+            ->sum(DB::raw('net_value_of_property + renovation_deployment - cost_of_sale_portion'));
+        $motorVehicle = MotorVehicle::where(['past_return' => 1,'user_id' => $userId])->sum('closing_cost');
+        $jewellery = Jewellery::where(['past_return' => 1,'user_id' => $userId])
+            ->sum(DB::raw('opening_value + purchase_value - sale_value'));
+        $furniture = FurnitureEquipment::where(['past_return' => 1,'user_id' => $userId])
+            ->sum(DB::raw('opening_value + purchase_value - sale_disposal_value'));
 
         $total = $partnershipBusiness + $businessAsset + $directoryShare + $nonAgriLand + $agriLand + $motorVehicle + $jewellery + $furniture;
 
